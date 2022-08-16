a82faf873abafcd664ea1110d2380c8f4ae6e13b
Илья <gosha.shlapak6982@gmail.com>
2022-08-16 15:37:46 +0300

    HomeWork5 исправления

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,56 @@ public class Main {
         }
         double [] weight2 = {1.57, 7.654, 9.986};
         int [] weight3 = {1, 2, 3, 4, 5};
-
-        for(int i=0; i< weight1.length-1; i++){
-            System.out.print(weight1[i]+",");
+//
+        for(int i=0; i<=weight1.length-1; i++) {
+            if (i != (weight1.length - 1)) {
+                System.out.print(weight1[i] + ",");
+            } else {
+                System.out.println(weight1[i]);
+            }
         }
-        System.out.println(weight1[(weight1.length - 1)]);
-        for(int i=0; i< weight2.length-1; i++){
+        //
+        for(int i=0; i<=weight2.length-1; i++){
             System.out.print(weight2[i]+",");
+            if (i != (weight2.length - 1)) {
+                System.out.print(weight2[i] + ",");
+            } else {
+                System.out.println(weight2[i]);
+            }
         }
-        System.out.println(weight2[(weight2.length - 1)]);
-        for(int i=0; i< weight3.length-1; i++){
-            System.out.print(weight3[i]+",");
+        //
+        for(int i=0; i<=weight3.length-1; i++){
+            if (i != (weight3.length - 1)) {
+                System.out.print(weight3[i] + ",");
+            } else {
+                System.out.println(weight3[i]);
+            }
         }
-        System.out.println(weight3[(weight3.length - 1)]);
+//
+        for(int i=(weight1.length-1); i>=0; i--){
+            if (i != 0) {
+                System.out.print(weight1[i] + ",");
+            } else {
+                System.out.println(weight1[i]);
+            }
+        }
 
-        for(int i=(weight1.length-1); i>0; i--){
-            System.out.print(weight1[i]+",");
-        }
-        System.out.println(weight1[0]);
 
-        for(int i=(weight2.length-1); i>0; i--){
-            System.out.print(weight2[i]+",");
+        for(int i=(weight2.length-1); i>=0; i--){
+            if (i != 0) {
+                System.out.print(weight2[i] + ",");
+            } else {
+                System.out.println(weight2[i]);
+            }
         }
-        System.out.println(weight2[0]);
 
-        for(int i=(weight3.length-1); i>0; i--){
-            System.out.print(weight3[i]+",");
+        for(int i=(weight3.length-1); i>=0; i--){
+            if (i != 0) {
+                System.out.print(weight3[i] + ",");
+            } else {
+                System.out.println(weight3[i]);
+            };
         }
-        System.out.println(weight3[0]);
 
         for(int i=0; i<=weight1.length-1; i++){
             if ((weight1[i]%2)==1){
